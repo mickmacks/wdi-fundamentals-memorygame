19177f97d08396d2527bc402f9350ff6d62f576a
Mahmoud Bachir <hey@mahmoudbachir.com>
2017-03-23 12:24:36 -0700

    added functions to game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,29 +1,64 @@
+// ROUND 3 - simplified functions.
+
+// Create an array to link cards and IDs
 var cards = ['queen', 'queen', 'king', 'king'];
 
+// Create an empty array to store flipped cards
 var cardsInPlay = [];
 
+//Create a function to check that 2 cards have been drawn, and alert if they match or not
 var checkForMatch = function () {
-	
+
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert('You found a match!')
-		} else {
-		alert('Sorry, try again.');
-		}
+      console.log("You found a match!");
+  	} else {
+      console.log("Sorry, try again.");
+ 	}
 }
 
+// Create a function to flip a card and store it in cardsInPlay array
 var flipCard = function (cardId) {
 
-console.log("User flipped " + cards[cardId]);
+	console.log('User flipped ' + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
 
-cardsInPlay.push(cards[cardId]);
-
-	if (cardsInPlay.length = 2) {
-		checkForMatch();
-	}
 }
 
 flipCard(0);
 flipCard(2);
+
+
+
+
+// // Round 2 - intergrating functions.
+
+// var cards = ['queen', 'queen', 'king', 'king'];
+
+// var cardsInPlay = [];
+
+// var checkForMatch = function () {
+	
+// 	if (cardsInPlay.length = 2) 
+// 	if (cardsInPlay[0] === cardsInPlay[1]) {
+// 		alert('You found a match!');
+// 		} else {
+// 		alert('Sorry, try again.');
+// 	}
+// }
+
+// var flipCard = function (cardId) {
+
+// 	console.log("User flipped " + cards[cardId]);
+// 	cardsInPlay.push(cards[cardId]); 
+// 	checkForMatch();
+	
+// }
+
+// flipCard(0);
+// flipCard(2);
+
+// // Round 1, no use of functions
 
 // console.log("Up and running!");
 
