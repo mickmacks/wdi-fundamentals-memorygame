f5af5a6ae0d37c4cf75ad212ae9c07f616dece13
Mahmoud Bachir <hey@mahmoudbachir.com>
2017-03-23 16:50:20 -0700

    working game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,32 @@
-// ROUND 3 - simplified functions.
+
+
+// ROUND 5 - Final working version
 
 // Create an array to link cards and IDs
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: 'images/queen-of-hearts.png'
+	},
+	{
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: 'images/queen-of-diamonds.png'
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: 'images/king-of-hearts.png'
+	},
+	{
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: 'images/king-of-diamonds.png'
+	}
+
+];
 
 // Create an empty array to store flipped cards
 var cardsInPlay = [];
@@ -9,24 +34,172 @@ var cardsInPlay = [];
 //Create a function to check that 2 cards have been drawn, and alert if they match or not
 var checkForMatch = function () {
 
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-      console.log("You found a match!");
-  	} else {
-      console.log("Sorry, try again.");
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+      	alert("You found a match!");
+  		} else {
+      	alert("Sorry, try again.");
+ 		}
  	}
 }
 
 // Create a function to flip a card and store it in cardsInPlay array
-var flipCard = function (cardId) {
+var flipCard = function () {
 
-	console.log('User flipped ' + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	var cardId = this.getAttribute('data-id');
+	console.log('User flipped ' + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
 
 }
 
-flipCard(0);
-flipCard(2);
+// Create a function to create a board and populate it with cards from the array.
+var createBoard = function () {
+
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement)[i];
+
+	}
+
+}
+
+createBoard();
+
+// // ROUND 4 - adding objects and click functions (doesn't work)
+
+// // Create an array to link cards and IDs
+// var cards = [
+
+// 	{
+// 		rank: 'queen',
+// 		suit: 'hearts',
+// 		cardImage: 'images/queen-of-hearts.png'
+// 	},
+// 	{
+// 		rank: 'queen',
+// 		suit: 'diamonds',
+// 		cardImage: 'images/queen-of-diamonds.png'
+// 	},
+// 	{
+// 		rank: 'king',
+// 		suit: 'hearts',
+// 		cardImage: 'images/king-of-hearts.png'
+// 	},
+// 	{
+// 		rank: 'king',
+// 		suit: 'diamonds',
+// 		cardImage: 'images/king-of-diamonds.png'
+// 	}
+
+// ];
+
+// // Create an empty array to store flipped cards
+// var cardsInPlay = [];
+
+// //Create a function to check that 2 cards have been drawn, and alert if they match or not
+// var checkForMatch = function () {
+
+// 	this.setAttribute('src', cards[cardId].cardImage);
+
+// 	if (cardsInPlay.length = 2);
+// 	if (cardsInPlay[0] === cardsInPlay[1]) {
+//       alert("You found a match!");
+//   	} else {
+//       alert("Sorry, try again.");
+//  	}
+// }
+
+// // Create a function to flip a card and store it in cardsInPlay array
+// var flipCard = function () {
+
+// 	var cardId = this.getAttribute('data-id');
+
+// 	console.log('User flipped ' + cards[cardId].rank);
+// 	cardsInPlay.push(cards[cardId].rank);
+// 	console.log(cards[cardId].cardImage);
+// 	console.log(cards[cardId].suit);
+// 	checkForMatch();
+
+// }
+
+// var createBoard = function () {
+
+// 	for (var i = 0; i < cards.length; i++) {
+// 		var cardElement = document.createElement('img');
+// 		cardElement.setAttribute('src', 'images/back.png');
+// 		cardElement.setAttribute('data-id', i);
+
+// 		cardElement[0].addEventListener('click', flipCard);
+// 		document.getElementById(game-board).appendChild('cardElement');
+// 	}
+// }
+
+// createBoard();
+
+
+// // ROUND 3 - Adding objects.
+
+// // Create an array to link cards and IDs
+// var cards = [
+
+// 	{
+// 		rank: 'queen',
+// 		suit: 'hearts',
+// 		cardImage: 'images/queen-of-hearts.png'
+// 	},
+// 	{
+// 		rank: 'queen',
+// 		suit: 'diamonds',
+// 		cardImage: 'images/queen-of-diamonds.png'
+// 	},
+// 	{
+// 		rank: 'king',
+// 		suit: 'hearts',
+// 		cardImage: 'images/king-of-hearts.png'
+// 	},
+// 	{
+// 		rank: 'king',
+// 		suit: 'diamonds',
+// 		cardImage: 'images/king-of-diamonds.png'
+// 	}
+
+// ];
+
+// // Create an empty array to store flipped cards
+// var cardsInPlay = [];
+
+// //Create a function to check that 2 cards have been drawn, and alert if they match or not
+// var checkForMatch = function () {
+
+// 	if (cardsInPlay.length = 2)
+// 	if (cardsInPlay[0] === cardsInPlay[1]) {
+//       alert("You found a match!");
+//   	} else {
+//       alert("Sorry, try again.");
+//  	}
+// }
+
+// // Create a function to flip a card and store it in cardsInPlay array
+// var flipCard = function (cardId) {
+
+// 	console.log('User flipped ' + cards[cardId].rank);
+// 	cardsInPlay.push(cards[cardId].rank);
+// 	console.log(cards[cardId].cardImage);
+// 	console.log(cards[cardId].suit);
+// 	checkForMatch();
+
+// }
+
+// flipCard(0);
+// flipCard(2);
 
 
 
